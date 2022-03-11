7a97d611d536d5bb6a065b0c5c2b1671fb25ed06
daniloinacio <daniloinaciossilva@gmail.com>
2022-03-10 23:33:13 -0300

    Ajustes no car de pets

--- src/components/PetCard/index.js
+++ src/components/PetCard/index.js
@@ -1,29 +1,57 @@
 import * as React from 'react';
-import { Image } from 'react-native';
-import { Text, Title } from 'react-native-paper';
-import { Container, ImageArea, TitleArea, DescriptionArea} from './styles';
+import { Title } from 'react-native-paper';
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import {TitleArea, ImageArea, Container, DescriptionArea, LineDescription} from './styles';
 import { FontAwesome } from '@expo/vector-icons';
+
+import image from '../../../assets/gato-feio-3.jpeg'
+import { backgroundColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
+// const image = { uri: "https://reactjs.org/logo-og.png" };
 
 const PetCard = ({name, sex, age, size, city, state, photo}) => {
   return (
-    <Container>
-      <TitleArea>
-        <Title>{name}</Title>
-        <FontAwesome name="heart-o" size={24} color="black" />
-      </TitleArea>
-      <ImageArea source={photo}>
-      </ImageArea>
-      <DescriptionArea>
-        <Text>{sex}</Text>
-        <Text>{age}</Text>
-        <Text>{size}</Text>
-      </DescriptionArea>
-      <DescriptionArea>
-        <Text>{city}</Text>
-        <Text>{state}</Text>
-      </DescriptionArea>
-    </Container>
+  <Container>
+    <TitleArea>
+      <Title>{name}</Title>
+      <FontAwesome name="heart-o" size={24} color="black" />
+    </TitleArea>
+    <ImageArea>
+      <View style={styles.container}>
+        <ImageBackground source={undefined} resizeMode="cover" style={styles.image}>
+        </ImageBackground>
+      </View>
+    </ImageArea>
+    <DescriptionArea>
+      <LineDescription>
+          <Text>{sex}</Text>
+          <Text>{age}</Text>
+          <Text>{size}</Text>
+      </LineDescription>
+      <LineDescription>
+          <Text>{city} - {state}</Text>
+      </LineDescription>
+    </DescriptionArea>
+  </Container>  
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+    height: 150
+  },
+  text: {
+    color: "white",
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#000000c0"
+  }
+});
 
 export default PetCard;
--- src/components/PetCard/styles.js
+++ src/components/PetCard/styles.js
@@ -3,26 +3,40 @@ import styled from "styled-components/native";
 export const Container = styled.View`
     display: flex;
     flex-direction: column;
-    background-color: #3f0;
-    borderRadius: 4;
+    marginTop: 10px;
+    width: 95%;
 `;
 
 export const TitleArea = styled.View`
     display: flex;
     flex-direction: row;
-    justify-content: space-around;
+    border-top-left-radius: 5;
+    border-top-right-radius: 5;
+    justify-content: space-between;
+    background-color: #fee29b;
     align-items: center;
+    paddingHorizontal: 16px;
 `;
 
-export const ImageArea = styled.Image`
-    width: 122px;
-    height: 44px;
-    margin-bottom: 10%;
+export const ImageArea = styled.View`
+    display: flex;
+    height: 150;
+`;
+export const LineDescription = styled.View`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    width: 100%;
+    align-items: center;
+    padding: 5px;
 `;
 
 export const DescriptionArea = styled.View`
     display: flex;
-    flex-direction: row;
-    justify-content: space-around;
+    flex-direction: column;
+    justify-content: center;
     align-items: center;
+    background-color: #fa8;
+    border-bottom-left-radius: 5;
+    border-bottom-right-radius: 5;
 `;
