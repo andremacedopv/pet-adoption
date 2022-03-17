97ddbbbb136c0f7a380722e06ac58cddeb06f441
André Macedo <andremacedo.pv@gmail.com>
2022-03-17 20:11:07 -0300

    correct routes

--- src/pages/InitialPage/index.js
+++ src/pages/InitialPage/index.js
@@ -49,10 +49,10 @@ const InitialPage = ({navigation}) => {
             <TextInfo>Qual o seu interesse?</TextInfo>
         </TextArea>
         <ButtonArea>
-            <Button type="normal" onPress={() => navigation.navigate("Ops")}>ADOTAR</Button>
+            <Button type="normal" onPress={() => navigation.navigate("Animais Disponíveis")}>ADOTAR</Button>
         </ButtonArea>
         <ButtonArea>
-            <Button type="normal">AJUDAR</Button>
+            <Button type="normal" onPress={() => navigation.navigate("Animais Disponíveis")}>AJUDAR</Button>
         </ButtonArea>
         <ButtonArea>
           {user? 
