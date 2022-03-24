b9360b3550273d6445eeff70665d16e9dd69aea0
André Macedo <andremacedo.pv@gmail.com>
2022-03-24 08:46:17 -0300

    :sparkles: Added drawer

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    useWindowDimensions,
+    Button,
+} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator, DrawerContentScrollView,
+    DrawerItemList,
+    DrawerItem, } from '@react-navigation/drawer';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { MenuButton } from './styles';
-import { Ionicons } from '@expo/vector-icons'; 
+import { Ionicons, Feather } from '@expo/vector-icons'; 
 import LoginPage from '../pages/LoginPage';
 import IndexAdoptPage from '../pages/IndexAdoptPage';
 import RegisterPage from '../pages/RegisterPage';
@@ -11,13 +21,108 @@ import InitialPage from '../pages/InitialPage';
 import AnimalRegisterPage from '../pages/AnimalRegisterPage';
 import MyPetsPage from '../pages/MyPetsPage';
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator()
+
+const Drawer = createDrawerNavigator();
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    menuContainer: {
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'space-evenly',
+    },
+    menuItemsCard: {
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 10,
+    },
+    circleContainer: {
+      width: 50,
+      height: 50,
+      borderRadius: 25,
+      padding: 10,
+    },
+  });
+
+function CustomDrawerContent(props) {
+    const width = useWindowDimensions().width * 0.3;
+  
+    return (
+      <DrawerContentScrollView {...props}>
+        <View style={styles.menuContainer}>
+          <View
+            style={[
+              styles.menuItemsCard,
+              { backgroundColor: '#fff2df', width: width, height: width },
+            ]}>
+            <>
+              <View
+                style={[styles.circleContainer, { backgroundColor: '#FFC56F' }]}>
+                <Feather travel name="briefcase" size={25} color="#fbae41" />
+                <DrawerItem
+                  label="Screen1"
+                  labelStyle={{ color: '#fbae41', fontSize: 10 }}
+                  onPress={() => {
+                    props.navigation.navigate('Cadastro do Animal', { body: 'hi' });
+                  }}
+                />
+              </View>
+            </>
+            <DrawerItem
+              style={{
+                position: 'absolute',
+                left: 0,
+                width: width,
+                height: width,
+              }}
+              label="Article"
+              labelStyle={{ color: '#609806' }}
+              onPress={() => {
+                props.navigation.navigate('Login', { body: 'article' });
+              }}
+            />
+          </View>
+          <View
+            style={[
+              styles.menuItemsCard,
+              { backgroundColor: '#EFFFD5', width: width, height: width },
+            ]}>
+            <View
+              style={[styles.circleContainer, { backgroundColor: '#b5ff39' }]}>
+              <Feather Medical name="briefcase" size={25} color="#609806" />
+            </View>
+  
+            <DrawerItem
+              style={{
+                position: 'absolute',
+                left: 0,
+                width: width,
+                height: width,
+              }}
+              label="Feed"
+              labelStyle={{ color: '#609806' }}
+              onPress={() => {
+                props.navigation.navigate('Página Inicial', { body: 'hello' });
+              }}
+            />
+          </View>
+        </View>
+      </DrawerContentScrollView>
+    );
+  }
 
 const Routes = () => {
   return (
     <NavigationContainer>
-        <Stack.Navigator
-            screenOptions={{
+        {/* <Drawer.Navigator initialRouteName="Home"> */}
+        <Drawer.Navigator
+            screenOptions={({ navigation }) => ({
                 headerStyle: {
                     backgroundColor: '#cfe9e5',
                 },
@@ -26,62 +131,64 @@ const Routes = () => {
                 fontWeight: 'bold',
                 },
                 headerLeft: () => (
-                    <MenuButton>
+                    <MenuButton onPress={() => navigation.openDrawer()}>
                         <Ionicons name="md-menu" size={30} color="#595959" />
                     </MenuButton>
                 )
-            }}
+            })}
+            drawerContent={(props) => <CustomDrawerContent {...props} />}
         >
-            <Stack.Screen
+            <Drawer.Screen
                 name="Página Inicial"
                 component={InitialPage}
                 options={{
-                    title: "",
+                    title: "Página Inicial",
                 }}
             />            
-            <Stack.Screen
+            <Drawer.Screen
                 name="SignUp"
                 component={RegisterPage}
                 options={{
                     title: "Cadastro Pessoal"
                 }}
             />
-            <Stack.Screen
+            <Drawer.Screen
                 name="Login"
                 component={LoginPage}    
                 options={{
                     title: "Login"
                 }}
             />
-            <Stack.Screen
+            <Drawer.Screen
                 name="Cadastro do Animal"
                 component={AnimalRegisterPage}
                 options={{
                     title: "Cadastro do Animal"
                 }}
             />
-            <Stack.Screen
+            <Drawer.Screen
                 name="Ops"
                 component={RegisterPageOps}
                 options={{
                     title: ""
                 }}
             />
-            <Stack.Screen
+            <Drawer.Screen
                 name="Animais Disponíveis"
                 component={IndexAdoptPage}
                 options={{
                     title: "Animais Disponíveis"
                 }}
             />
-            <Stack.Screen
+            <Drawer.Screen
                 name="Meus Pets"
                 component={MyPetsPage}
                 options={{
                     title: "",
                 }}
             />
-        </Stack.Navigator>
+        </Drawer.Navigator>
+        {/* </Drawer.Navigator> */}
     </NavigationContainer>
   );
 };
