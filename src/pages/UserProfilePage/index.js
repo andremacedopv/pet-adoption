101667a43a2b45196d3913026b8c997937579016
daniloinacio <daniloinaciossilva@gmail.com>
2022-03-30 22:29:41 -0300

    Implement basic structur from user profile page

--- src/pages/UserProfilePage/index.js
+++ src/pages/UserProfilePage/index.js
@@ -2,7 +2,7 @@ import {useState, useEffect} from "react";
 import Button from './../../components/Button'
 import { database, storage } from "../../services/firebase"
 import { doc, getDoc } from "firebase/firestore";
-import {Container} from './styles';
+import {Container, Info, InfoRow, InfoSection, InfoArea, Image, Field, FieldTitle} from './styles';
 import { ActivityIndicator, ScrollView, Alert} from 'react-native';
 
 const UserProfilePage = ({route, navigation}) => {
@@ -13,7 +13,62 @@ const UserProfilePage = ({route, navigation}) => {
     return (
        <ScrollView>
            <Container>
-                
+                <Image>
+                </Image>                
+                <InfoArea>
+                    <InfoRow>Nome</InfoRow>
+                    <InfoSection>
+                        <Info>
+                            <FieldTitle>NOME COMPLETO</FieldTitle>
+                            <Field></Field>
+                        </Info>
+                    </InfoSection>
+                    <InfoSection>
+                        <Info>
+                            <FieldTitle>IDADE</FieldTitle>
+                            <Field></Field>
+                        </Info>
+                    </InfoSection>
+                    <InfoSection>
+                        <Info>
+                            <FieldTitle>EMAIL</FieldTitle>
+                            <Field></Field>
+                        </Info>
+                    </InfoSection>
+                    <InfoSection>
+                        <Info>
+                            <FieldTitle>LOCALIZAÇÃO</FieldTitle>
+                            <Field></Field>
+                        </Info>
+                    </InfoSection>
+                    <InfoSection>
+                        <Info>
+                            <FieldTitle>ENDEREÇO</FieldTitle>
+                            <Field></Field>
+                        </Info>
+                    </InfoSection>
+                    <InfoSection>
+                        <Info>
+                            <FieldTitle>TELEFONE</FieldTitle>
+                            <Field></Field>
+                        </Info>
+                    </InfoSection>
+                    <InfoSection>
+                        <Info>
+                            <FieldTitle>NOME DE USUÁRIO</FieldTitle>
+                            <Field></Field>
+                        </Info>
+                    </InfoSection>
+                    <InfoSection>
+                        <Info>
+                            <FieldTitle>HISTÓRICO</FieldTitle>
+                            </Info>
+                    </InfoSection>
+                </InfoArea>
+                <ButtonArea>
+                    <Button>CHAT</Button>
+                    <Button>HISTÓRIAS</Button>
+                </ButtonArea>
            </Container>
        </ScrollView> 
     );
