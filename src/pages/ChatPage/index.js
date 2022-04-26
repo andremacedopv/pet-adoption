d3c94fdcad9a6894c8d4a8920845d369449949ce
Felipe <felipevaz444@gmail.com>
2022-04-26 12:18:25 -0300

    :sparkles: Chat simples funcionando

--- src/pages/ChatPage/index.js
+++ src/pages/ChatPage/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import {GiftedChat} from 'react-native-gifted-chat'
 import {database} from '../../services/firebase'
 import { useUserContext } from "../../contexts/useUserContext";
@@ -9,28 +9,46 @@ const ChatPage = () => {
   const [messages, setMessages] = useState([])
   const { userData } = useUserContext();
   
-  useEffect(() => {
-    const subscribe = database.collection('chatId').onSnapshot((snapshot) => {
-      snapshot.docChanges().forEach((change) => {
-        if(change.type == "added"){
-          let data = change.doc.data()
-          data.createdAt = data.createdAt.toDate()
-          setMessages(prevMessages => {
-            GiftedChat.append(prevMessages, data )
-          })
-        }
-      })
-    })
-    return () => subscribe()
-  },[])
-  
+  // useEffect(() => {
+  //   const subscribe = database.collection('chatId').onSnapshot((snapshot) => {
+  //     snapshot.docChanges().forEach((change) => {
+  //       if(change.type == "added"){
+  //         let data = change.doc.data()
+  //         data.createdAt = data.createdAt.toDate()
+  //         setMessages(prevMessages => {
+  //           GiftedChat.append(prevMessages, data )
+  //         })
+  //       }
+  //     })
+  //   })
+  //   return () => subscribe()
+  // },[])
 
-  const onSend = (messages) => {
-    database
-    .collection('chatId')
-    .doc(Date.now().toString())
-    .set(messages[0])
-  }
+  useEffect(() => {
+    setMessages([
+        {
+            _id: 1,
+            text: 'Hello developer',
+            createdAt: new Date(),
+            user: {
+                _id: 2,
+                name: 'React Native',
+                avatar: 'https://placeimg.com/140/140/any',
+            },
+        },
+    ])
+  }, []);
+  
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+  }, []);
+
+  // const onSend = (messages) => {
+  //   database
+  //   .collection('chatId')
+  //   .doc(Date.now().toString())
+  //   .set(messages[0])
+  // }
 
   return( 
     <Container>
